26f0a88f76f0783791e413cbcdab7d1da316c5e9
lubor bodzioch <48643500+luborbodzioch@users.noreply.github.com>
2019-03-17 11:01:38 +0100

    Update Program.cs

--- GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
+++ GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
@@ -11,6 +11,32 @@ namespace GraMonolitycznie
         static void Main(string[] args)
         {
             Console.WriteLine("witaj");
+            Console.WriteLine("Podaj imie:");
+            string imie;
+            imie = Console.ReadLine();
+
+            Console.WriteLine("podaj nazwisko");
+            string nazwisko = Console.ReadLine();
+
+            Console.WriteLine("Witaj " + imie + " " + nazwisko);
+
+            Console.WriteLine("Podaj wiek");
+            int wiek = Convert.ToInt32(Console.ReadLine());
+
+            if (wiek < 67)
+            {
+                Console.WriteLine("Do emerytury zostalo ci {0} lat", 67 - wiek);
+            }
+            else
+            {
+                Console.WriteLine("Jestes na emeryturze");
+            }
+
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
